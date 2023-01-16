5493f022bd699ebecb29b721b10f2cfa27927a0f
Dzmitry_Kazlou <dzmitry_kazlou1@epam.com>
2023-01-16 23:45:32 +0300

    POC of eslint rule wrote

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,15 +1,14 @@
 interface Params {
-    id: number;
+  id: number;
 }
 
 interface UpdateBookBodyDto {
-    name?: string;
-    author?: string;
+  name?: string;
+  author?: string;
 }
 
 interface BookResponse {
-    id: number;
-    name: string;
-    author: string;
+  id: number;
+  name: string;
+  author: string;
 }
-
